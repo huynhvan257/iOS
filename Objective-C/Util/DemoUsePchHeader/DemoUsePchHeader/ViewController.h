0eae32d23ac2ebb5869610266c36c23f162c55a1
VanHT <vfa.vanht@gmail.com>
2016-02-12 16:44:23 +0900

    ! demo add file pct

---
+++ Objective-C/Util/DemoUsePchHeader/DemoUsePchHeader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoUsePchHeader
+//
+//  Created by VanHT on 2/12/16.
+//  Copyright © 2016 VanHT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Objective-C/Util/DemoUsePchHeader/DemoUsePchHeader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoUsePchHeader
+//
+//  Created by VanHT on 2/12/16.
+//  Copyright © 2016 VanHT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
